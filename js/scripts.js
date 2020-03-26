50c403d78afbce41479c75bce65835f30e900f24
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2020-03-26 17:29:42 -0600

    about page ready

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+let menuIsOpen = false
+const menu = document.getElementById('menu')
+const menuBtn = document.getElementById('menu-btn')
+
+const closeMenu = () => {
+  menu.className = menu.className.replace('left-50', 'left-100')
+  menuBtn.innerHTML = '<img src="img/toggle_menu_open-02.png" alt="toggle menu open">'
+  menuIsOpen = false
+}
+
+const openMenu = () => {
+  menu.className = menu.className.replace('left-100', 'left-50')
+  menuBtn.innerHTML = '<img src="img/toggle_menu_close-03.png" alt="toggle menu close">'
+  menuIsOpen = true
+}
+
+window.onload = () => {
+  // bind click handler to menu button
+  menuBtn.onclick = (e) => {
+    e.preventDefault()
+    if (menuIsOpen) closeMenu()
+    else openMenu()
+  }
+}
+
+window.onresize = closeMenu
