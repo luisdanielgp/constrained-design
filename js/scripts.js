c3baf1f5ea1e54f4e2074397fc211d6a44fb8b6c
luisdanielgp <luisdaniel.guzmanp@gmail.com>
2020-03-27 13:15:35 -0600

    links on navbar fixed

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,13 @@ const menuBtn = document.getElementById('menu-btn')
 
 const closeMenu = () => {
   menu.className = menu.className.replace('left-50', 'left-100')
-  menuBtn.innerHTML = '<img src="img/toggle_menu_open-02.png" alt="toggle menu open">'
+  menuBtn.innerHTML = '<img src="/img/toggle_menu_open-02.png" alt="toggle menu open">'
   menuIsOpen = false
 }
 
 const openMenu = () => {
   menu.className = menu.className.replace('left-100', 'left-50')
-  menuBtn.innerHTML = '<img src="img/toggle_menu_close-03.png" alt="toggle menu close">'
+  menuBtn.innerHTML = '<img src="/img/toggle_menu_close-03.png" alt="toggle menu close">'
   menuIsOpen = true
 }
 
